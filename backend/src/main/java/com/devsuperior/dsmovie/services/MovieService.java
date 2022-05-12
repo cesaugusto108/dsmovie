f9ec3d88ea0302a0dd89b09aac89a95fe86d3f79
César Augusto <ces.augusto108@gmail.com>
2022-05-11 22:26:27 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -26,6 +26,7 @@ public class MovieService {
     public MovieDTO findById(Long id) {
         Movie result = repository.findById(id).get();
         MovieDTO dto = new MovieDTO(result);
+
         return dto;
     }
 }
