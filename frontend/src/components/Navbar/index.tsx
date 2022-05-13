40cd7253afe6a0ef0355e20d460c55f1e6f47400
César Augusto <ces.augusto108@gmail.com>
2022-05-13 17:39:57 -0300

    First request

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import { ReactComponent as MovieIcon } from "assets/img/film.svg";
 import "./styles.css";
 
 function Navbar() {
@@ -6,7 +7,12 @@ function Navbar() {
             <header>
                   <nav className="container">
                         <div className="dsmovie-nav-content">
-                              <h1 className="display-5">DSMovie</h1>
+                              <a href="/">
+                                    <div className="dsmovie-logo">
+                                          <MovieIcon className="dsmovie-film-icon" />
+                                          <h1 className="display-5 ms-3">DSMovie</h1>
+                                    </div>
+                              </a>
                               <a href="https://github.com/cesaugusto108">
                                     <GithubIcon className="dsmovie-contact-link-icon" />
                                     <span className="dsmovie-contact-link">/devsuperior</span>
